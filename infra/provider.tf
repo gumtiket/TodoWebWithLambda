00905db6b6c745990b9e19851f197573a6716f7f
gumtiket <gumtiket0303@gmail.com>
2026-08-30 13:42:53 +0900

    fix: complete provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,1 +1,14 @@
-2i
+terraform {
+  required_version = ">= 1.15" # terraform 버전은 1.15 이상
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+}
